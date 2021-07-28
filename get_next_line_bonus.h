3bc26d025720eab7df836a30208902fcbf19d953
Seung hee Lee <slee@c8r8s2.42seoul.kr>
2021-07-28 14:10:21 +0900

    gnl

---
+++ get_next_line_bonus.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slee <slee@student.42.kr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/05 18:52:05 by slee              #+#    #+#             */
+/*   Updated: 2021/07/28 13:53:31 by slee             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 256
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+int		free_backup(char **backup);
+char	*print_line(char *line, char **backup, int size, char *tmp);
+char	*get_next_line(int fd);
+
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_malloc(size_t size);
+
+#endif
